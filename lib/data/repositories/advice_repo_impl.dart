79f3fa38d79f1646744239f13f9835b8f51bdaa6
wilfred gicharu <wilfredgcharu254@gmail.com>
2023-06-01 23:03:57 +0300

    application layer

---
+++ lib/data/repositories/advice_repo_impl.dart
@@ -0,0 +1,16 @@
+import '../../domain/repositories/advice_repo.dart';
+
+class AdviceRepoImpl implements AdviceRepo {
+  @override
+  Future <Either<Failure, AdviceEntity>> getAdviceFromDatasource() async {
+    try {
+      final result = await adviceRemoteDatasource.getRandomAdviceFromApi();
+      return right(result);
+    } on ServerException catch (_) {
+      return left(ServerFailure());
+    } catch (e) {
+      return left(GeneralFailure());
+    }
+  }
+
+}
