09f266a3d270291ddbf5562582f44535a9e1192e
wilfred gicharu <wilfredgcharu254@gmail.com>
2023-06-02 23:00:15 +0300

    adding datasource

--- lib/data/repositories/advice_repo_impl.dart
+++ lib/data/repositories/advice_repo_impl.dart
@@ -1,17 +1,15 @@
-// import '../../domain/repositories/advice_repo.dart';
-//
-// class AdviceRepoImpl implements AdviceRepo {
-//   AdviceRepoImpl({required this.})
-//   @override
-//   Future <Either<Failure, AdviceEntity>> getAdviceFromDatasource() async {
-//     try {
-//       final result = await adviceRemoteDatasource.getRandomAdviceFromApi();
-//       return right(result);
-//     } on ServerException catch (_) {
-//       return left(ServerFailure());
-//     } catch (e) {
-//       return left(GeneralFailure());
-//     }
-//   }
-//
-// }
+import 'package:advise/data/datasources/advice_remote_datasource.dart';
+import 'package:advise/domain/entities/advice_entity.dart';
+import 'package:advise/domain/failures/failures.dart';
+import 'package:advise/domain/repositories/advice_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class AdviceRepoImpl implements AdviceRepo{
+  final AdviceRemoteDatasource adviceRemoteDataSource = AdviceRemoteDatasourceImpl();
+  @override
+  Future<Either<Failure, AdviceEntity>> getAdviceFromDatasource() async {
+    final result = await adviceRemoteDataSource.getRandomAdviceFromApi();
+    return right(result);
+  }
+
+}
--- lib/domain/usecases/advice_use_cases.dart
+++ lib/domain/usecases/advice_use_cases.dart
@@ -1,11 +1,12 @@
+import 'package:advise/data/repositories/advice_repo_impl.dart';
 import 'package:advise/domain/entities/advice_entity.dart';
 import 'package:advise/domain/failures/failures.dart';
 import 'package:dartz/dartz.dart';
 
 class AdviceUseCases{
+  final adviceRepo = AdviceRepoImpl();
+
   Future<Either<Failure, AdviceEntity>> getAdvice() async{
-    await Future.delayed(Duration(seconds: 3), (){});
-    // return right(const AdviceEntity(advice: "advice to test", id: 1));
-    return left(ServerFailure());
+     return adviceRepo.getAdviceFromDatasource();
   }
 }
